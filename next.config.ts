4167592ba877b1c0257a45541d637488abdb5fb6
Yupi <youngperchik2404@gmail.com>
2026-09-16 18:37:37 +0500

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'PretextProj';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
